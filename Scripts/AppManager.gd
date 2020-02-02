2f8fc8e2b2d6d46a8615be3dc47010cf2d435b24
komehara <komehara@gamedesignshort.com>
2020-02-02 15:45:30 +0000

    [SCREEN] Support hi-dpi. Press H to toggle 2x window size.

--- Scripts/AppManager.gd
+++ Scripts/AppManager.gd
@@ -1,5 +1,28 @@
 extends Node
 
+# Parameters
+var initial_size = Vector2(ProjectSettings.get_setting("display/window/size/width"),
+	ProjectSettings.get_setting("display/window/size/height"))
+
+# State
+var hidpi_active: bool
+
 func _input(event):
+	# let user toggle hi-dpi resolution freely
+	# (hi-dpi is hard to detect and resize is hard to force on start)
+	if event.is_action_pressed("toggle_hidpi"):
+		toggle_hidpi()
+		
 	if event.is_action_pressed("app_exit"):
 		get_tree().quit()
+
+func toggle_hidpi():
+	if hidpi_active:
+		# back to normal size
+		OS.set_window_size(initial_size)
+	else:
+		# set hi-dpi size (nothing more than 2x window size)
+		OS.set_window_size(initial_size * 2)
+		
+	# toggle
+	hidpi_active = not hidpi_active
