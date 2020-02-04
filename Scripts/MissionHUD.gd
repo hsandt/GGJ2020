68368d10108d4272351fffc9db33f3ac00cacbb3
Hyper Sonic <hs@fuwar.in>
2020-02-04 00:49:51 +0000

    [DEBUG] Fixed MissionHUD elements not properly hidden when starting from Mission scene

--- Scripts/MissionHUD.gd
+++ Scripts/MissionHUD.gd
@@ -3,23 +3,24 @@ extends Control
 export(NodePath) var title
 
 func _ready():
-	# DEBUG: when debugging by starting in MissionHUD scene, current mission is 0
-	# so set something meaningful to avoid errors (there is no level though,
-	# so you won't be able to test everything as in the real game)
-	if GameManager.current_mission_number == 0:
-		GameManager.current_mission_number = 1
-		GameManager.phase = GameManager.Phase.Setup
-	# END DEBUG
-	
 	var _error
 	_error = GameManager.connect("setup_mission", self, "on_mission_setup")
 	_error = GameManager.connect("run_mission", self, "on_mission_run")
 	_error = GameManager.connect("succeed_mission", self, "on_mission_succeed")
 	_error = GameManager.connect("fail_mission", self, "on_mission_failed")
-		
+	
 	var title_label = get_node(self.title) as Label
 	if title_label:
 		title_label.text = "Mission %02d" % GameManager.current_mission_number
+		
+	# DEBUG: when debugging by starting in MissionHUD scene, current mission is 0
+	# so setup something meaningful to avoid errors (there is no level though,
+	# so you won't be able to test everything as in the real game)
+	# must be done after connecting signals so setup_mission comes back to us
+	if GameManager.current_mission_number == 0:
+		GameManager.current_mission_number = 1
+		GameManager.setup_mission()
+	# END DEBUG
 
 func on_mission_setup():
 	$Success.hide()
