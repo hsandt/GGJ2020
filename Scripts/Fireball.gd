9fa8bc8fe3b435d3e56714d72ddc92e53e7d94dd
Hyper Sonic <hs@fuwar.in>
2020-02-04 00:49:17 +0000

    [CLEANUP] Fixed warnings

--- Scripts/Fireball.gd
+++ Scripts/Fireball.gd
@@ -4,9 +4,9 @@ class_name Fireball
 # Parameter
 var velocity : Vector2
 
-func setup(position, velocity):
+func setup(position, setup_velocity):
 	self.position = position
-	self.velocity = velocity
+	self.velocity = setup_velocity
 
-func _process(delta):
-	move_and_slide(self.velocity)
+func _process(_delta):
+	self.velocity = move_and_slide(self.velocity)
