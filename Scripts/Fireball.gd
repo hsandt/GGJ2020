0e6582277796c1823971097dfef5e615dc4cfaae
Hyper Sonic <hs@fuwar.in>
2020-02-09 19:59:24 +0000

    [FIREBALL] Added timer to disable collider on spawn
    to avoid killing Mage Red

--- Scripts/Fireball.gd
+++ Scripts/Fireball.gd
@@ -1,12 +1,21 @@
 extends KinematicBody2D
 class_name Fireball
 
-# Parameter
+onready var shape = $CollisionShape2D as CollisionShape2D
+
+# Setup parameter
 var velocity : Vector2
 
 func setup(position, setup_velocity):
 	self.position = position
 	self.velocity = setup_velocity
+	
+	# prevent killing character shooting this fireball / being blocked on spawn
+	shape.disabled = true
+
+func _on_Timer_timeout():
+	# after short time, re-enable collider
+	shape.disabled = false
 
 func _process(delta):
 	var kinematic_collision2d = move_and_collide(self.velocity * delta)
